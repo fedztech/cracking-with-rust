3b713167e9da0575b1023dd2de6f8259d1a5ce85
Juan Carlos Garza Fernandez <fedztech@gmail.com>
2024-11-18 15:16:31 +0000

    Text of ex 1.4

---
+++ src/ex_01_04.rs
@@ -0,0 +1,8 @@
+// Palindrome permutation
+// Given a string, write a function to check if iti s a permutation of a palindrome.
+// A palindrome is a word or phrase that is the same forwards and backwards.
+// A permutation is a rearrangement of letters. 
+// The palindrome does not need to be limited to just dictionary words.
+// EXAMPLE:
+// Input: Tact Coa
+// Output: True
